11eb43ffa71d2ab46e2f3b3e55ff660236fdf05f
ruanshudong <ruanshudong@qq.com>
2021-11-30 11:36:37 +0800

    TarsCpp & TarsWeb use submodule

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/TarsCloud/TarsCpp --recursive
-git clone https://github.com/TarsCloud/TarsWeb build/TarsWeb
+git submodule update --init --recursive
