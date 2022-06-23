6c96ae5e8c68c2352266199f17fef05ed7430265
ruanshudong <ruanshudong@qq.com>
2022-06-23 15:56:57 +0800

    add compiler/tools/exec-buildx-cloud.sh, use docker buildx

--- compiler/tools/exec-buildx-cloud.sh
+++ compiler/tools/exec-buildx-cloud.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 生成上到云端应用市场的镜像, 根据value.yaml, 
+# 根据自己的Dockefile生成多平台的镜像, 自己Dockerfile里面完成代码的编译以及镜像准备过程
 # 自动生成版本号
 # 自动生成镜像地址
 
@@ -8,14 +8,21 @@
 # Automatically generate version number
 # Automatically generate image address
 
-if [ $# -lt 2 ]; then
-    echo "Usage: $0 YamlFile Tag"
-    echo "for example, $0 yaml/values.yaml latest "
+if [ $# -lt 4 ]; then
+    echo "Usage: $0 SERVERTYPE(cpp/nodejs/java-war/java-jar/go/php) YamlFile Tag Dockerfile"
+    echo "for example, $0 nodejs yaml/values.yaml latest Dockerfile"
     exit -1
 fi
 
-VALUES=$1
-TAG=$2
+SERVERTYPE=$1
+VALUES=$2
+TAG=$3
+Dockerfile=$4
+
+if [ "$SERVERTYPE" != "cpp" ] && [ "$SERVERTYPE" != "nodejs" ] && [ "$SERVERTYPE" != "java-war" ] && [ "$SERVERTYPE" != "java-jar" ] && [ "$SERVERTYPE" != "go" ] && [ "$SERVERTYPE" != "php" ] ; then  
+    echo "Usage: $0 SERVERTYPE(cpp/nodejs/java-war/java-jar/go/php)"
+    exit -1
+fi
 
 #-------------------------------------------------------------------------------------------
 
@@ -29,22 +36,14 @@ if [ -z $TAG ]; then
     exit -1
 fi
 
-NODEJS_SHELL="/root/yaml-tools/index"
-
-GROUP="`node $NODEJS_SHELL -f $VALUES -g cloud.group`"
-NAME="`node $NODEJS_SHELL -f $VALUES -g cloud.name`"
-LOGO="`node $NODEJS_SHELL -f $VALUES -n -g cloud.logo`"
-CHANGELIST="`node $NODEJS_SHELL -f $VALUES -n -g cloud.changelist`"
-README="`node $NODEJS_SHELL -f $VALUES -n -g cloud.readme`"
-README_CN="`node $NODEJS_SHELL -f $VALUES -n -g cloud.readme_cn`"
-ASSETS="`node $NODEJS_SHELL -f $VALUES -n -g cloud.assets`"
-SERVERS="`node $NODEJS_SHELL -f $VALUES -n -g cloud.servers`"
-TYPE="`node $NODEJS_SHELL -f $VALUES -n -g cloud.type`"
-
-if [ "$TYPE" != "product" ] ; then
-    echo "type must be product, exit."
-    exit -1
-fi
+GROUP="`node /root/yaml-tools/index -f $VALUES -g cloud.group`"
+NAME="`node /root/yaml-tools/index -f $VALUES -g cloud.name`"
+LOGO="`node /root/yaml-tools/index -f $VALUES -n -g cloud.logo`"
+CHANGELIST="`node /root/yaml-tools/index -f $VALUES -n -g cloud.changelist`"
+TARS="`node /root/yaml-tools/index -f $VALUES -n -g cloud.protocols`"
+README="`node /root/yaml-tools/index -f $VALUES -n -g cloud.readme`"
+README_CN="`node /root/yaml-tools/index -f $VALUES -n -g cloud.readme_cn`"
+ASSETS="`node /root/yaml-tools/index -f $VALUES -n -g cloud.assets`"
 
 if [ -z $GROUP ]; then
     echo "group in ${VALUES} must not be empty"
@@ -69,28 +68,37 @@ fi
 IMAGE="docker.tarsyun.com/${GROUP}/${NAME}:${TAG}"
 
 # update values image
-node $NODEJS_SHELL -f $VALUES -s cloud.image -v $IMAGE -u
-node $NODEJS_SHELL -f $VALUES -s cloud.version -v $TAG -u
-node $NODEJS_SHELL -f $VALUES -s cloud.group -v $GROUP -u
+node /root/yaml-tools/index -f $VALUES -s repo.image -v $IMAGE -u
+node /root/yaml-tools/index -f $VALUES -s cloud.version -v $TAG -u
+node /root/yaml-tools/index -f $VALUES -s cloud.deploy -v $VALUES -u
+node /root/yaml-tools/index -f $VALUES -s cloud.group -v $GROUP -u
+node /root/yaml-tools/index -f $VALUES -s cloud.name -v $NAME -u
 
 echo "---------------------Environment---------------------------------"
 echo "VALUES:               "$VALUES
+echo "SERVERTYPE:           "$SERVERTYPE
+echo "PUSH:                 "$PUSH
 echo "TAG:                  "$TAG
 echo "GROUP:                "$GROUP
 echo "NAME:                 "$NAME
 echo "LOGO:                 "$LOGO
 echo "IMAGE:                "$IMAGE
-echo "SERVERS:              "$SERVERS
+echo "TARS:                 "$TARS
 echo "README:               "$README
 echo "README_CN:            "$README_CN
 echo "ASSETS:               "$ASSETS
 echo "CHANGELIST:           "$CHANGELIST
 echo "----------------------Build docker--------------------------------"
 
-NewDockerfile="Dockerfile.tmp"
+NewDockerfile=${Dockerfile}.new
 
-echo "FROM ubuntu:20.04" > ${NewDockerfile}
+cp -rf ${Dockerfile} ${NewDockerfile}
+
 echo "COPY $VALUES /usr/local/cloud/cloud.yaml" >> ${NewDockerfile}
+
+for KEY in ${TARS}; do
+    echo "COPY $KEY /usr/local/cloud/data/$KEY" >> ${NewDockerfile}
+done
 
 if [ "$README" != "" ]; then
     if [ ! -f $README ] ; then
@@ -133,11 +141,11 @@ for KEY in ${ASSETS}; do
     echo "COPY $KEY /usr/local/cloud/data/$KEY" >> ${NewDockerfile}
 done
 
-echo "docker buildx build . -f ${NewDockerfile} -t $IMAGE --platform=linux/amd64,linux/arm64 --push"
+
+echo "docker buildx build . -f ${NewDockerfile} -t $IMAGE -platform=linux/amd64,linux/arm64"
+
 docker buildx build . -f ${NewDockerfile} -t $IMAGE --platform=linux/amd64,linux/arm64 --push
 
 rm -rf ${NewDockerfile}
-
-# docker push $IMAGE
 
 
