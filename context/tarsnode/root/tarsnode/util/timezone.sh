5d5c04f16d3a75ebe890760c09cbaf5471d9ccbf
zhixiongdu027 <root@libssl.com>
2022-09-16 15:08:10 +0800

    fix timezone compare

--- context/tarsnode/root/tarsnode/util/timezone.sh
+++ context/tarsnode/root/tarsnode/util/timezone.sh
@@ -11,13 +11,11 @@ if cd /usr/share/zoneinfo; then
 
     _ZONE_FILES_ARRAY_=(${_ZONE_FILES_})
 
-    _LOCALTIME_ZONE_MD5_=$(zdump -v "${_LOCALTIME_FILE_}" | awk '{print $2$3$4$6$7$8$9$10$11$12$13$14$15$16}' | md5sum)
+    _LOCALTIME_ZONE_MD5_=$(zdump -c 2010,2030 "${_LOCALTIME_FILE_}" | awk '{print $2$3$4$6$7$8$9$10$11$12$13$14$15$16}' | md5sum)
     for ZONEFILE in "${_ZONE_FILES_ARRAY_[@]}"; do
-      _MD5_=$(zdump -v "${ZONEFILE}" | awk '{print $2$3$4$6$7$8$9$10$11$12$13$14$15$16}' | md5sum)
+      _MD5_=$(zdump -c 2010,2030 "${ZONEFILE}" | awk '{print $2$3$4$6$7$8$9$10$11$12$13$14$15$16}' | md5sum)
       if [ "$_MD5_" = "$_LOCALTIME_ZONE_MD5_" ]; then
         echo "Will Update TimeZone To" "${ZONEFILE}"
-        rm -rf "${_LOCALTIME_FILE_}"
-        ln -sf /usr/share/zoneinfo/"${ZONEFILE}" "${_LOCALTIME_FILE_}"
         echo "${ZONEFILE}" >"${_TIMEZONE_FILE_}"
         export TZ=${ZONEFILE}
         break
